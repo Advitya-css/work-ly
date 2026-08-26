78d95334053bac3e93f4389650b154ad46a68f37
“Advitya <advityabansal@gmail.com>
2026-08-26 20:12:04 +0530

    chore: clean up patch scripts

--- patch_location_text.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/app/(app)/settings/page.tsx', 'utf8');
-
-code = code.replace(
-  'Where you are, and everywhere else you would take a job. Used to filter and rank what\n            Workly shows you.',
-  'Where you are, and everywhere else you would take a job.'
-);
-
-fs.writeFileSync('src/app/(app)/settings/page.tsx', code);
--- patch_remote_text.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/settings/location-settings-form.tsx', 'utf8');
-
-code = code.replace(
-  'When this is on, a remote role always counts as matching your locations, wherever it is\n            based.',
-  'Remote roles will automatically count as a location match.'
-);
-
-fs.writeFileSync('src/components/settings/location-settings-form.tsx', code);
