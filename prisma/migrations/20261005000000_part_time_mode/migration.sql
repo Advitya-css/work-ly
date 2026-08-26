67b2b8e1d4f8bda0aa3d905dd40fad9a7914f677
“Advitya <advityabansal@gmail.com>
2026-08-26 12:25:20 +0530

    fix: correct table name in part time migration

--- prisma/migrations/20261005000000_part_time_mode/migration.sql
+++ prisma/migrations/20261005000000_part_time_mode/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE "CareerProfile" ADD COLUMN "isPartTimeMode" BOOLEAN NOT NULL DEFAULT false,
+ALTER TABLE "career_profiles" ADD COLUMN "isPartTimeMode" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN "availability" TEXT;
