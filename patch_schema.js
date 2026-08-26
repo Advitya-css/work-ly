a45a17927a4cf8859aa4f9be4a8d51617f1058f9
“Advitya <advityabansal@gmail.com>
2026-08-27 00:42:24 +0530

    chore: remove patch scripts

--- patch_schema.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let schema = fs.readFileSync('prisma/schema.prisma', 'utf8');
-
-schema = schema.replace(
-  '  updatedAt     DateTime @updatedAt',
-  '  updatedAt     DateTime @updatedAt\n\n  isPro         Boolean  @default(false)\n  lastAlertSentAt DateTime?'
-);
-
-fs.writeFileSync('prisma/schema.prisma', schema);
